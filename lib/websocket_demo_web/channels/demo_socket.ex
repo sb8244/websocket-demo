19b2b31637065d43bc326cde6181cbcbff8606fb
Stephen Bussey <sb8244@cs.ship.edu>
2018-02-10 20:50:36 -0500

    remove fluff

--- lib/websocket_demo_web/channels/demo_socket.ex
+++ lib/websocket_demo_web/channels/demo_socket.ex
@@ -6,32 +6,12 @@ defmodule WebsocketDemoWeb.DemoSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
+  # Allow any socket to connect; this should be behind authentication most likely
   def connect(_params, socket) do
     {:ok, socket}
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
-  #
-  #     def id(socket), do: "user_socket:#{socket.assigns.user_id}"
-  #
-  # Would allow you to broadcast a "disconnect" event and terminate
-  # all active sockets and channels for a given user:
-  #
-  #     WebsocketDemoWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
-  #
-  # Returning `nil` makes this socket anonymous.
+  # No need to identify sockets here, as there is no authentication
   def id(_socket), do: nil
 end
