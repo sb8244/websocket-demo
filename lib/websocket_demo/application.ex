8677ae895ef150a05079cf1f440f1d713c7c2600
Stephen Bussey <sb8244@cs.ship.edu>
2018-02-10 20:43:38 -0500

    no need for ecto

--- lib/websocket_demo/application.ex
+++ lib/websocket_demo/application.ex
@@ -8,8 +8,6 @@ defmodule WebsocketDemo.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
-      supervisor(WebsocketDemo.Repo, []),
       # Start the endpoint when the application starts
       supervisor(WebsocketDemoWeb.Endpoint, []),
       # Start your own worker by calling: WebsocketDemo.Worker.start_link(arg1, arg2, arg3)
