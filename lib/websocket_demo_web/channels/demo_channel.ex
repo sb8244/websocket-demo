512358046ee516292b2540c48879ea490b6dd417
Stephen Bussey <sb8244@cs.ship.edu>
2018-02-10 20:56:16 -0500

    ping/pong implemented
    The client can send a ping request and receive a pong back

--- lib/websocket_demo_web/channels/demo_channel.ex
+++ lib/websocket_demo_web/channels/demo_channel.ex
@@ -2,7 +2,13 @@ defmodule WebsocketDemoWeb.DemoChannel do
   require Logger
   use Phoenix.Channel
 
+  intercept ["ping"]
+
   def join("demo:" <> _id, _params, socket) do
     {:ok, socket}
+  end
+
+  def handle_in("ping", _params, socket) do
+    {:reply, {:ok, %{response: "pong"}}, socket}
   end
 end
--- test/websocket_demo_web/channels/demo_channel_test.exs
+++ test/websocket_demo_web/channels/demo_channel_test.exs
@@ -1,3 +1,27 @@
 defmodule WebsocketDemoWeb.DemoChannelTest do
   use WebsocketDemoWeb.ChannelCase, async: true
+
+  alias WebsocketDemoWeb.DemoChannel
+
+  describe "join demo:id" do
+    test "any socket can join the channel" do
+      {:ok, _, socket} =
+        socket(nil, %{})
+        |> subscribe_and_join(DemoChannel, "demo:1")
+
+      leave(socket)
+    end
+  end
+
+  describe "handle_in ping" do
+    test "a pong response is returned" do
+      {:ok, _, socket} =
+        socket(nil, %{})
+        |> subscribe_and_join(DemoChannel, "demo:1")
+
+      ref = push(socket, "ping", %{})
+      assert_reply(ref, :ok, %{response: "pong"}, 1000)
+      leave(socket)
+    end
+  end
 end
